25f0d1c64d8d822787ee630e569c29a569264ffc
MainShayne233 <shaynetremblay@gmail.com>
2019-10-05 12:34:40 -0400

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Vlad.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10-dev",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -15,9 +16,16 @@ defmodule Vlad.MixProject do
     []
   end
 
+  defp dialyzer do
+    [
+      plt_file: {:no_warn, "priv/plts/vlad.plt"}
+    ]
+  end
+
   defp deps do
     [
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
+      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false}
     ]
   end
 end
