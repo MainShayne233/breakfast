6ab6c491111416735f5be34f77d280df100d66d7
MainShayne233 <shaynetremblay@gmail.com>
2019-10-13 15:34:00 -0400

    Add helper for isolating tests in modules!

--- test/breakfast_test.exs
+++ test/breakfast_test.exs
@@ -1,129 +1,134 @@
-defmodule Client do
-  use Breakfast
+defmodule BreakfastTest do
+  use ExUnit.Case
+  import TestHelper
+  doctest Breakfast
 
-  defdecoder User do
-    field(:email, String.t())
-    field(:age, integer(), cast: &Client.int_from_string/1)
-    field(:timezone, String.t(), default: "US")
-    field(:roles, [String.t()])
+  testmodule Client do
+    use Breakfast
+    alias __MODULE__.User
 
-    field(:status, String.t(),
-      parse: fn
-        %{"UserStatus" => "Pending"} -> {:ok, "Pending"}
-        %{"UserStatus" => "Approved"} -> "Approved"
+    defdecoder User do
+      field(:email, String.t())
+      field(:age, integer(), cast: &Client.int_from_string/1)
+      field(:timezone, String.t(), default: "US")
+      field(:roles, [String.t()])
+
+      field(:status, String.t(),
+        parse: fn
+          %{"UserStatus" => "Pending"} -> {:ok, "Pending"}
+          %{"UserStatus" => "Approved"} -> "Approved"
+          _other -> :error
+        end
+      )
+    end
+
+    def int_from_string(value) when is_binary(value) do
+      case Integer.parse(value) do
+        {int, ""} -> {:ok, int}
         _other -> :error
       end
-    )
-  end
+    end
 
-  def int_from_string(value) when is_binary(value) do
-    case Integer.parse(value) do
-      {int, ""} -> {:ok, int}
-      _other -> :error
+    def int_from_string(_), do: :error
+
+    setup do
+      params = %{
+        "email" => "shayne@hotmail.com",
+        "age" => "10",
+        "UserStatus" => "Pending",
+        "roles" => ["user", "admin"]
+      }
+
+      %{params: params}
+    end
+
+    test "should succeed for valid params", %{params: params} do
+      assert match?({:ok, %User{}}, User.decode(params))
+    end
+
+    test "should result in a parse error if a field is missing", %{params: params} do
+      assert User.decode(Map.delete(params, "age")) ==
+               {:error,
+                %Breakfast.DecodeError{
+                  message: "Could not parse field from params",
+                  type: :parse_error,
+                  value: :age
+                }}
+    end
+
+    test "should result in a parse error if the custom parse function returns :error", %{
+      params: params
+    } do
+      assert User.decode(Map.put(params, "UserStatus", "Canclled")) ==
+               {:error,
+                %Breakfast.DecodeError{
+                  message: "Could not parse field from params",
+                  type: :parse_error,
+                  value: :status
+                }}
+    end
+
+    test "should raise a runtime exception if the custom parse returns a bad value", %{
+      params: params
+    } do
+      assert assert_raise(Breakfast.DecodeError, fn ->
+               User.decode(Map.put(params, "UserStatus", "Approved"))
+             end) == %Breakfast.DecodeError{
+               message:
+                 "The parse function defined for the field returned an invald type. Expected {:ok, term()} | :error, but got: \"Approved\"",
+               type: :bad_parse_return,
+               value: [field: :status, bad_return: "Approved"]
+             }
+    end
+
+    test "should complain about invalid value for field", %{params: params} do
+      assert User.decode(Map.put(params, "email", :shayneAThotmailDOTcom)) ==
+               {:error,
+                %Breakfast.DecodeError{
+                  message: "Invalid value for field",
+                  type: :validate_error,
+                  value: {:email, :shayneAThotmailDOTcom}
+                }}
+    end
+
+    test "should complain about a bad cast", %{params: params} do
+      assert User.decode(Map.put(params, "age", :"10")) ==
+               {:error,
+                %Breakfast.DecodeError{
+                  message: "Value failed to cast",
+                  type: :cast_error,
+                  value: {:age, :"10"}
+                }}
     end
   end
 
-  def int_from_string(_), do: :error
-end
+  testmodule InferValidator do
+    use Breakfast
 
-defmodule BreakfastTest do
-  use ExUnit.Case
-  alias Client.User
-  doctest Breakfast
+    test "should give a helpful error if unable to infer the validator for a custom type" do
+      assert assert_raise(Breakfast.CompileError, fn ->
+               defmodule Client do
+                 use Breakfast
+                 @type status :: :approved | :pending | :rejected
 
-  setup do
-    params = %{
-      "email" => "shayne@hotmail.com",
-      "age" => "10",
-      "UserStatus" => "Pending",
-      "roles" => ["user", "admin"]
-    }
-
-    %{params: params}
-  end
-
-  test "should succeed for valid params", %{params: params} do
-    assert match?({:ok, %User{}}, User.decode(params))
-  end
-
-  test "should result in a parse error if a field is missing", %{params: params} do
-    assert User.decode(Map.delete(params, "age")) ==
-             {:error,
-              %Breakfast.DecodeError{
-                message: "Could not parse field from params",
-                type: :parse_error,
-                value: :age
-              }}
-  end
-
-  test "should result in a parse error if the custom parse function returns :error", %{
-    params: params
-  } do
-    assert User.decode(Map.put(params, "UserStatus", "Canclled")) ==
-             {:error,
-              %Breakfast.DecodeError{
-                message: "Could not parse field from params",
-                type: :parse_error,
-                value: :status
-              }}
-  end
-
-  test "should raise a runtime exception if the custom parse returns a bad value", %{
-    params: params
-  } do
-    assert assert_raise(Breakfast.DecodeError, fn ->
-             User.decode(Map.put(params, "UserStatus", "Approved"))
-           end) == %Breakfast.DecodeError{
-             message:
-               "The parse function defined for the field returned an invald type. Expected {:ok, term()} | :error, but got: \"Approved\"",
-             type: :bad_parse_return,
-             value: [field: :status, bad_return: "Approved"]
-           }
-  end
-
-  test "should complain about invalid value for field", %{params: params} do
-    assert User.decode(Map.put(params, "email", :shayneAThotmailDOTcom)) ==
-             {:error,
-              %Breakfast.DecodeError{
-                message: "Invalid value for field",
-                type: :validate_error,
-                value: {:email, :shayneAThotmailDOTcom}
-              }}
-  end
-
-  test "should complain about a bad cast", %{params: params} do
-    assert User.decode(Map.put(params, "age", :"10")) ==
-             {:error,
-              %Breakfast.DecodeError{
-                message: "Value failed to cast",
-                type: :cast_error,
-                value: {:age, :"10"}
-              }}
-  end
-
-  test "should give a helpful error if unable to infer the validator for a custom type" do
-    assert assert_raise(Breakfast.CompileError, fn ->
-             defmodule Client2 do
-               use Breakfast
-               @type status :: :approved | :pending | :rejected
-
-               defdecoder Request do
-                 field(:statuses, Client.status())
+                 defdecoder Request do
+                   field(:statuses, Client.status())
+                 end
                end
-             end
-           end)
+             end)
 
-    defmodule Client2 do
-      use Breakfast
-      @type status :: :approved | :pending | :rejected
+      assert (defmodule Client do
+                use Breakfast
+                @type status :: :approved | :pending | :rejected
 
-      defdecoder Request do
-        field(:statuses, [Client.status()])
+                defdecoder Request do
+                  field(:statuses, [Client.status()])
 
-        validate(Client.status(), fn value ->
-          if value in [:approved, :pending, :rejected], do: :ok, else: :error
-        end)
-      end
+                  validate(Client.status(), fn value ->
+                    if value in [:approved, :pending, :rejected], do: :ok, else: :error
+                  end)
+                end
+              end)
     end
   end
 end
