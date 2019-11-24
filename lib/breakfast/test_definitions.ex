51dac5c8c86a33ba7fabe1cd138438bb46494af6
MainShayne233 <shaynetremblay@gmail.com>
2019-11-23 19:09:36 -0500

    Only render test definitions in :test

--- lib/breakfast/test_definitions.ex
+++ lib/breakfast/test_definitions.ex
@@ -2,19 +2,21 @@
 # this is code used by tests to run certain assertions. it is being compiled
 # in lib versus within a test module due to the way Elixir throws out doc/type information
 # w/ test modules
-defmodule Breakfast.TestDefinitions do
-  @moduledoc false
+if Mix.env() == :test do
+  defmodule Breakfast.TestDefinitions do
+    @moduledoc false
 
-  defmodule Struct do
-    defstruct [:name]
+    defmodule Struct do
+      defstruct [:name]
+    end
+
+    defmodule OtherStruct do
+      defstruct [:email]
+    end
+
+    @type status :: :approved | :pending | :rejected
+
+    @type rgb_color :: :red | :green | :blue
+    @type cmyk_color :: :cyan | :magenta | :yellow | :black
   end
-
-  defmodule OtherStruct do
-    defstruct [:email]
-  end
-
-  @type status :: :approved | :pending | :rejected
-
-  @type rgb_color :: :red | :green | :blue
-  @type cmyk_color :: :cyan | :magenta | :yellow | :black
 end
